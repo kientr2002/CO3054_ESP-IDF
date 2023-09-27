5fd4144fb15edd6885dab8744eb08bbabfac9137
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-09-27 10:48:13 +0700

    restructure program, rename some variable and add comment

--- Lab-2/main/main.c
+++ Lab-2/main/main.c
@@ -8,38 +8,39 @@
 
 
 #define DEBOUNCE_TIME 50
-int LED2_STATE = 0; //led 2 is use to identify that Task1 is acive and this is the led's state
-static uint32_t millis()
+#define LED_ON 1
+#define LED_OFF 0
+
+static uint32_t current_ms()
 {
     // get current time (ms)
     return esp_timer_get_time() / 1000;
 }
-// Task 1
+// Task 1 Default every second
+int LED2_STATE = 0; //led 2 is use to identify that Task1 is acive and this is the led's state
 void print_studentID(void *pvParameter)
 {
     //setup pin 2 for led.
     esp_rom_gpio_pad_select_gpio(GPIO_NUM_2);
     gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT);
 
-    for (;;)
+    while (1)
     {
+        /* code */
         // print student ID and time (s)
-        printf("Task1: 2013552 2013223, time: %lld(s)\n", esp_timer_get_time() / 1000000);
+        printf("%lld s: 2013552 2013223\n", esp_timer_get_time() / 1000000);
         gpio_set_level(GPIO_NUM_2, LED2_STATE);
         //Reverse led 2 state
-        if(LED2_STATE){
-            LED2_STATE = 0;
-        } else {
-            LED2_STATE = 1;
-        }
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-        
+        LED2_STATE = LED2_STATE?LED_OFF:LED_ON;
+        vTaskDelay(1000 / portTICK_PERIOD_MS); //1s
     }
+    
 }
 // Task 2 When the button pressed
+
 void read_button(void *param)
     {
-    // setup pin 19 for button
+    // setup pin 19 for button (pull-up)
     esp_rom_gpio_pad_select_gpio(GPIO_NUM_17);
     gpio_set_direction(GPIO_NUM_17, GPIO_MODE_INPUT);
     gpio_set_pull_mode(GPIO_NUM_17, GPIO_PULLUP_ONLY);
@@ -48,9 +49,10 @@ void read_button(void *param)
     esp_rom_gpio_pad_select_gpio(GPIO_NUM_5);
     gpio_set_direction(GPIO_NUM_5, GPIO_MODE_OUTPUT);
 
-    int lastSteadyState = 1;      // the previous steady state from the input pin
-    int lastFlickerableState = 1; // the previous flickerable state from the input pin
-    int currentState;             // the current reading from the input pin
+    //initial variable
+    int previousStableState = 1; //Using after DEBOUNCE_TIME
+    int previousState = 1; //Every times
+    int currentState;
 
     uint32_t lastDebounceTime = 0; // the last time input pin was toggled
 
@@ -61,31 +63,27 @@ void read_button(void *param)
         currentState = gpio_get_level(GPIO_NUM_17);
 
         // If the button changed, due to noise or pressing:
-        if (currentState != lastFlickerableState)
+        if (currentState != previousState)
         {
-            // reset the debouncing timer
-            lastDebounceTime = millis();
-            // save the last flickable state
-            lastFlickerableState = currentState;
+            lastDebounceTime = current_ms();
+            previousState = currentState;
         }
 
         // whatever the reading is at, it's been there for longer than the debounce
         // delay, so take it as the actual current state:
-        if ((millis() - lastDebounceTime) > DEBOUNCE_TIME)
+        if ((current_ms() - lastDebounceTime) > DEBOUNCE_TIME)
         {
             // if the button state has changed
-            if (lastSteadyState == 1 && currentState == 0)
+            if (previousStableState == 1 && currentState == 0)
             {
-                gpio_set_level(GPIO_NUM_5, 1);
-
+                gpio_set_level(GPIO_NUM_5, LED_ON);
                 printf("ESP32\n");
             }
-                else if (lastSteadyState == 0 && currentState == 1)
+                else if (previousStableState == 0 && currentState == 1)
             {
-                gpio_set_level(GPIO_NUM_5, 0);
+                gpio_set_level(GPIO_NUM_5, LED_OFF);
             }
-            // save the last steady state
-            lastSteadyState = currentState;
+            previousStableState = currentState;
         }
         vTaskDelay(20 / portTICK_PERIOD_MS);
     }
@@ -94,6 +92,6 @@ void read_button(void *param)
 
 void app_main(void)
 {
-    xTaskCreate(print_studentID, "Print student ID", 2048, NULL, 5, NULL);
-    xTaskCreate(read_button, "Reading from button", 2048, NULL, 6, NULL);
+    xTaskCreate(print_studentID, "Task 1", 2048, NULL, 5, NULL);
+    xTaskCreate(read_button, "Task 2", 2048, NULL, 6, NULL);
 }
