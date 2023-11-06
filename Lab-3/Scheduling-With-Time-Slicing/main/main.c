afc5cd4de8ef5a3b197b914f8958b98b5f952042
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-11-06 08:24:50 +0700

    update lab3 ex1

--- Lab-3/Scheduling-With-Time-Slicing/main/main.c
+++ Lab-3/Scheduling-With-Time-Slicing/main/main.c
@@ -10,43 +10,50 @@
 
 
 volatile uint32_t ulIdleCycleCount = 0UL;
-volatile uint16_t task1CycleCount = 0UL;
-volatile uint16_t task2CycleCount = 0UL;
-volatile uint16_t task3CycleCount = 0UL;
+volatile uint32_t task1CycleCount = 0UL;
+volatile uint32_t task2CycleCount = 0UL;
+volatile uint32_t task3CycleCount = 0UL;
 
 
 void vApplicationIdleHook(void) {
     ulIdleCycleCount++;
-    printf("======================================\n");
-    printf("Idle task\n");
-    printf("======================================\n");
-    
 }
-
+ void printCounter(){
+    printf("--------------------------------------\n");
+    printf("Task1 is Counting: %ld/1000000\n",task1CycleCount);
+    printf("Task2 is Counting: %ld/2000000\n",task2CycleCount);
+    printf("Task3 is Counting: %ld/3000000\n",task3CycleCount);
+    printf("Idle count: %ld\n", ulIdleCycleCount);
+    printf("--------------------------------------\n");
+ }
 /*
     Task 1: (high, event)
-    Print the counting from 1 to 1000
+    Print the counting from 1 to 1000000
     Print the time (ms) after the count terminate
 */
 void Task1(void){
     printf("======================================\n");
     printf("Task 1 will be start!\n");
+    printf("Task's counter before start\n");
+    printCounter();
     printf("======================================\n");
     int64_t begin_time = esp_timer_get_time();
     while (1)
     {
         /* code */
         task1CycleCount++;
-        if(task1CycleCount > 1000){
+        if(task1CycleCount >= 1000000){
             int64_t end_time = esp_timer_get_time();
             printf("======================================\n");
             printf("Task 1 terminate during: %lld ms\n", (end_time-begin_time)/1000);
             printf("======================================\n");
             printf("Task 1 ended!\n");
+            printf("Result:\n");
+            printCounter();
             printf("======================================\n");
             vTaskDelete(NULL);
         } else {
-            printf("Task1 is Counting: %d\n",task1CycleCount);
+            // printf("Task1 is Counting: %d\n",task1CycleCount);
         }
         
     }
@@ -55,48 +62,77 @@ void Task1(void){
 }
 /*
     Task 2: (Idle priority, continuous)
-    Print the counting from 1 to 2000
+    Print the counting from 1 to 2000000
     Print the time (ms) after the count terminate
 */
 void Task2(void){
     printf("======================================\n");
     printf("Task 2 will be start!\n");
+    printf("Task's counter before start\n");
+    printCounter();
     printf("======================================\n");
     int64_t begin_time = esp_timer_get_time();
     while (1)
     {
         /* code */
         task2CycleCount++;
-        if(task2CycleCount > 2000){
+        if(task2CycleCount >= 2000000){
             int64_t end_time = esp_timer_get_time();
             printf("======================================\n");
             printf("Task 2 terminate during: %lld ms\n", (end_time-begin_time)/1000);
             printf("======================================\n");
             printf("Task 2 ended!\n");
+            printf("Result:\n");
+            printCounter();
             printf("======================================\n");
             vTaskDelete(NULL);
         } else {
-            printf("Task2 is Counting: %d\n",task2CycleCount);
+            // printf("Task2 is Counting: %d\n",task2CycleCount);
         }
         
     }
 }
-
+/*
+    Task 3: (Idle priority, continuous)
+    Print the counting from 1 to 3000000
+    Print the time (ms) after the count terminate
+*/
 void Task3(void){
     printf("======================================\n");
     printf("Task 3 will be start!\n");
+    printf("Task's counter before start\n");
+    printCounter();
     printf("======================================\n");
-
-    //TODO
-
-    printf("======================================\n");
-    printf("Task 3 ended!\n");
-    printf("======================================\n");
-    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    int64_t begin_time = esp_timer_get_time();
+    while (1)
+    {
+        /* code */
+        task3CycleCount++;
+        if(task3CycleCount >= 3000000){
+            int64_t end_time = esp_timer_get_time();
+            printf("======================================\n");
+            printf("Task 3 terminate during: %lld ms\n", (end_time-begin_time)/1000);
+            printf("======================================\n");
+            printf("Task 3 ended!\n");
+            printf("Result:\n");
+            printCounter();
+            printf("======================================\n");
+            vTaskDelete(NULL);
+        } else {
+            // printf("Task3 is Counting: %d\n",task1CycleCount);
+        }
+        
+    }
+       
+    
 }
 void app_main() {
-    xTaskCreatePinnedToCore(Task1, "Task1", 2048, NULL, 1, NULL, 0);
-    xTaskCreatePinnedToCore(Task2, "Task2", 2048, NULL, 2, NULL, 0);
-    vTaskDelay(10000 / portTICK_PERIOD_MS);
     esp_register_freertos_idle_hook_for_cpu(vApplicationIdleHook, 0);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    xTaskCreatePinnedToCore(Task1, "Task1", 5120, NULL, tskIDLE_PRIORITY, NULL, 0);
+    xTaskCreatePinnedToCore(Task2, "Task2", 5120, NULL, tskIDLE_PRIORITY, NULL, 0);
+    vTaskDelay(100 / portTICK_PERIOD_MS);
+    xTaskCreatePinnedToCore(Task3, "Task3", 5120, NULL, 5, NULL, 0);
+    
 }
+
