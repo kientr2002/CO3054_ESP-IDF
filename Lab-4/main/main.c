f04e6d657f86d068ca7f1e253cbd61a68a3950b0
kientr2002 <kien.tranvictory@hcmut.edu.vn>
2023-11-12 16:41:04 +0700

    complete lab 4

--- Lab-4/main/main.c
+++ Lab-4/main/main.c
@@ -10,7 +10,8 @@
 #include "esp_timer.h"
 #include <freertos/queue.h>
 
-#define QUEUE_CAPACITY 4
+#define QUEUE_CAPACITY 3
+
 
 QueueHandle_t xQueue;
 //message structure
@@ -28,8 +29,7 @@ typedef struct {
 float random_Number(int32_t num1, int32_t num2)
 {
     float random = ((float)rand()) / (float)RAND_MAX;
-    float r = random * (num2 - num1);
-    return num1 + r;
+    return num1 + random * (num2 - num1);
 }
 void not_send_Error(){
   printf("Do not send the data into the queue!!!\n");
@@ -48,16 +48,19 @@ void task_1_sender(void *pvParameter){
   while (1)
   {
     /* code */
-    task1Buffer.eDataID = TASK1;
-    task1Buffer.lDataValue = random_Number(2010000, 2019999);
-    task1Status = xQueueSendToBack(xQueue, &task1Buffer, task1Wait);
-    if(task1Status == pdPASS){
-      printf("Task 1: The Student's Identification is %ld\n", task1Buffer.lDataValue);
+    if(uxQueueMessagesWaiting(xQueue) < QUEUE_CAPACITY){
+      task1Buffer.eDataID = TASK1;
+      task1Buffer.lDataValue = random_Number(2010000, 2019999);
+      task1Status = xQueueSendToBack(xQueue, &task1Buffer, task1Wait);
+      if(task1Status == pdPASS){
+        printf("Task 1 sended\n");
+      } else {
+        not_send_Error();
+      }
     } else {
-      // Print error
-      not_send_Error();
+      full_Queue_Error();
     }
-    vTaskDelay(50 / portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }
   
 }
@@ -71,16 +74,19 @@ void task_2_sender(void *pvParameter){
   while (1)
   {
     /* code */
-    task2Buffer.eDataID = TASK2;
-    task2Buffer.lDataValue = random_Number(0, 10);
-    task2Status = xQueueSendToBack(xQueue, &task2Buffer, task2Wait);
-    if(task2Status == pdPASS){
-      printf("Task 2: The Student's GPA is %ld\n", task2Buffer.lDataValue);
+    if(uxQueueMessagesWaiting(xQueue) < QUEUE_CAPACITY){
+      task2Buffer.eDataID = TASK2;
+      task2Buffer.lDataValue = random_Number(0, 10);
+      task2Status = xQueueSendToBack(xQueue, &task2Buffer, task2Wait);
+      if(task2Status == pdPASS){
+        printf("Task 2 sended\n");
+      } else {
+        not_send_Error();
+      }
     } else {
-      // Print error
-      not_send_Error();
+      full_Queue_Error();
     }
-    vTaskDelay(50 / portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }  
 }
 
@@ -93,40 +99,58 @@ void task_3_sender(void *pvParameter){
   while (1)
   {
     /* code */
-    task3Buffer.eDataID = TASK3;
-    task3Buffer.lDataValue = random_Number(0, 10);
-    task3Status = xQueueSendToBack(xQueue, &task3Buffer, task3Wait);
-    if(task3Status == pdPASS){
-      printf("Task 3: The Student's class number is %ld\n", task3Buffer.lDataValue);
+    if(uxQueueMessagesWaiting(xQueue) < QUEUE_CAPACITY){
+      task3Buffer.eDataID = TASK3;
+      task3Buffer.lDataValue = random_Number(101, 199);
+      task3Status = xQueueSendToBack(xQueue, &task3Buffer, task3Wait);
+      if(task3Status == pdPASS){
+        printf("Task 3 sended\n");
+      } else {
+        // Print error
+        not_send_Error();
+      }
     } else {
-      // Print error
-      not_send_Error();
+      full_Queue_Error();
     }
-    vTaskDelay(50 / portTICK_PERIOD_MS);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
   }  
 }
 
 //Receive Task
 void receive_Task(void *pvParameter){
   Data receive;
-
+  int zero_flag = 0;
   while (1)
   {
+
     /* code */
-    if(uxQueueMessagesWaiting(xQueue) == QUEUE_CAPACITY){
+    printf("=========================================================\n");
+    if(uxQueueMessagesWaiting(xQueue) > QUEUE_CAPACITY){
       //Print error
       full_Queue_Error();
+    }else {
+      if(uxQueueMessagesWaiting(xQueue)){
+        printf("The Current Queue Capacity is %d/%d \n",uxQueueMessagesWaiting(xQueue), QUEUE_CAPACITY);
+        zero_flag = 0;
+      } else {
+        if(!zero_flag){
+          printf("The Current Queue Capacity is %d/%d \n",uxQueueMessagesWaiting(xQueue), QUEUE_CAPACITY);
+          zero_flag = 1;
+        } 
+      }         
+      
+      if(xQueueReceive(xQueue, &receive, 0) == pdPASS){
+        if(receive.eDataID == TASK1){
+          printf("Task 1: The Student's Identification is %ld\n",receive.lDataValue);
+        } else if(receive.eDataID == TASK2){
+          printf("Task 2: The Student's GPA is %ld\n",receive.lDataValue);
+        } else if(receive.eDataID == TASK3){
+          printf("Task 3: The Student's class number is %ld\n",receive.lDataValue);
+        } 
+      }
     }
-    if(xQueueReceive(xQueue, &receive, 0) == pdPASS){
-      // if(receive.eDataID == TASK1){
-      //   printf("1\n");
-      // } else if(receive.eDataID == TASK2){
-      //   printf("2\n");
-      // } else if(receive.eDataID == TASK3){
-      //   printf("3\n");
-      // } 
-    }
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+    printf("=========================================================\n"); 
+    vTaskDelay(200 / portTICK_PERIOD_MS);
   }
   
 }
@@ -137,11 +161,11 @@ void app_main(void){
   xQueue = xQueueCreate(QUEUE_CAPACITY, sizeof(Data));
   if(xQueue != NULL){
     //TODO
-    printf("The queue created successfully!!!\n");
-    xTaskCreatePinnedToCore(&task_1_sender, "task1", 1024 * 5, NULL, 2, NULL, 0);
-    xTaskCreatePinnedToCore(&task_2_sender, "task2", 1024 * 5, NULL, 2, NULL, 0);
-    xTaskCreatePinnedToCore(&task_3_sender, "task3", 1024 * 5, NULL, 2, NULL, 0);
-    xTaskCreatePinnedToCore(&receive_Task, "receive", 1024 * 5, NULL, 1, NULL, 0);
+    printf("The queue created successfully!!!\n");  
+    xTaskCreatePinnedToCore(&task_1_sender, "task1", 5120, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(&task_2_sender, "task2", 5120, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(&task_3_sender, "task3", 5120, NULL, 2, NULL, 0);
+    xTaskCreatePinnedToCore(&receive_Task, "receive", 5120, NULL, 1, NULL, 0);
   } else {
     printf("The queue created unsuccessfully!!!\n");
   }
